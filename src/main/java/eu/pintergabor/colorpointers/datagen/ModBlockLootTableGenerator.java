8046ec5f395cd2f6f208b9c496f2c9f979060940
Pintér Gábor <pinter.gabor@gmx.at>
2024-11-20 19:34:00 +0100

    1.0.1+mc1.20.5

--- src/main/java/eu/pintergabor/colorpointers/datagen/ModBlockLootTableGenerator.java
+++ src/main/java/eu/pintergabor/colorpointers/datagen/ModBlockLootTableGenerator.java
@@ -1,26 +1,29 @@
 package eu.pintergabor.colorpointers.datagen;
 
-import static eu.pintergabor.colorpointers.main.Main.arrowMarks;
-
 import eu.pintergabor.colorpointers.blocks.ArrowMarkBlock;
 import eu.pintergabor.colorpointers.items.ArrowMarkItem;
 import eu.pintergabor.colorpointers.main.ArrowMarkVariant;
-
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+import static eu.pintergabor.colorpointers.main.Main.arrowMarks;
 
 public class ModBlockLootTableGenerator extends FabricBlockLootTableProvider {
-	public ModBlockLootTableGenerator(FabricDataOutput dataOutput) {
-		super(dataOutput);
-	}
+    public ModBlockLootTableGenerator(
+            FabricDataOutput dataOutput, CompletableFuture<RegistryWrapper.WrapperLookup> registryLookup) {
+        super(dataOutput, registryLookup);
+    }
 
-	/**
-	 * Every {@link ArrowMarkBlock} drops the corresponding {@link ArrowMarkItem}
-	 */
-	@Override
-	public void generate() {
-		for (ArrowMarkVariant arrowMark : arrowMarks) {
-			addDrop(arrowMark.block, arrowMark.item);
-		}
-	}
+    /**
+     * Every {@link ArrowMarkBlock} drops the corresponding {@link ArrowMarkItem}
+     */
+    @Override
+    public void generate() {
+        for (ArrowMarkVariant arrowMark : arrowMarks) {
+            addDrop(arrowMark.block, arrowMark.item);
+        }
+    }
 }
--- src/main/java/eu/pintergabor/colorpointers/main/ArrowMarkVariant.java
+++ src/main/java/eu/pintergabor/colorpointers/main/ArrowMarkVariant.java
@@ -1,13 +1,13 @@
 package eu.pintergabor.colorpointers.main;
 
-import static eu.pintergabor.colorpointers.Global.arrowMarkBlockLumi;
-
 import eu.pintergabor.colorpointers.blocks.ArrowMarkBlock;
 import eu.pintergabor.colorpointers.items.ArrowMarkItem;
 import eu.pintergabor.colorpointers.util.ModIdentifier;
-
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.piston.PistonBehavior;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -15,57 +15,55 @@ import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockView;
 
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import static eu.pintergabor.colorpointers.Global.arrowMarkBlockLumi;
 
 /**
  * One ArrowMark variant
  */
 public class ArrowMarkVariant {
 
-	/**
-	 * ArrowMark block
-	 * <p>
-	 * Read only outside class.
-	 */
-	public ArrowMarkBlock block;
+    /**
+     * ArrowMark block
+     * <p>
+     * Read only outside class.
+     */
+    public ArrowMarkBlock block;
 
-	/**
-	 * ArrowMark item
-	 * <p>
-	 * Read only outside class.
-	 */
-	public ArrowMarkItem item;
+    /**
+     * ArrowMark item
+     * <p>
+     * Read only outside class.
+     */
+    public ArrowMarkItem item;
 
-	/**
-	 * Create one variant of ArrowMark
-	 */
-	public ArrowMarkVariant(String name) {
-		// Block
-		block = new ArrowMarkBlock(FabricBlockSettings
-			.create()
-			.replaceable()
-			.noCollision()
-			.nonOpaque()
-			.sounds(BlockSoundGroup.MOSS_CARPET)
-			.luminance(arrowMarkBlockLumi)
-			.postProcess(ArrowMarkVariant::always)
-			.emissiveLighting(ArrowMarkVariant::always)
-			.pistonBehavior(PistonBehavior.DESTROY)
-		);
-		Registry.register(Registries.BLOCK, new ModIdentifier(name), block);
-		// Item
-		item = new ArrowMarkItem(block, new FabricItemSettings());
-		Registry.register(Registries.ITEM, new ModIdentifier(name), item);
-		// Item groups
-		ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(
-			entries -> {
-				entries.add(item);
-			});
-	}
+    /**
+     * Create one variant of ArrowMark
+     */
+    public ArrowMarkVariant(String name) {
+        // Block
+        block = new ArrowMarkBlock(FabricBlockSettings
+                .create()
+                .replaceable()
+                .noCollision()
+                .nonOpaque()
+                .sounds(BlockSoundGroup.MOSS_CARPET)
+                .luminance(arrowMarkBlockLumi)
+                .postProcess(ArrowMarkVariant::always)
+                .emissiveLighting(ArrowMarkVariant::always)
+                .pistonBehavior(PistonBehavior.DESTROY)
+        );
+        Registry.register(Registries.BLOCK, new ModIdentifier(name), block);
+        // Item
+        item = new ArrowMarkItem(block, new Item.Settings());
+        Registry.register(Registries.ITEM, new ModIdentifier(name), item);
+        // Item groups
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(
+                entries -> {
+                    entries.add(item);
+                });
+    }
 
-	private static boolean always(BlockState blockState, BlockView blockView, BlockPos blockPos) {
-		return true;
-	}
+    private static boolean always(BlockState blockState, BlockView blockView, BlockPos blockPos) {
+        return true;
+    }
 }
