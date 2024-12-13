256394a102f54701642a8547e3c8a2168dc12e69
Pintér Gábor <pinter.gabor@gmx.at>
2024-12-13 17:47:20 +0100

    1.0.1+mc1.21.4

--- src/main/java/eu/pintergabor/colorpointers/datagen/ModModelProvider.java
+++ src/main/java/eu/pintergabor/colorpointers/datagen/ModModelProvider.java
@@ -1,39 +1,37 @@
 package eu.pintergabor.colorpointers.datagen;
 
+import eu.pintergabor.colorpointers.main.ArrowMarkVariant;
+import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.minecraft.client.data.BlockStateModelGenerator;
+import net.minecraft.client.data.ItemModelGenerator;
+import net.minecraft.client.data.Models;
+
 import static eu.pintergabor.colorpointers.main.Main.arrowMarks;
 
-import eu.pintergabor.colorpointers.main.ArrowMarkVariant;
-
-import net.minecraft.data.client.BlockStateModelGenerator;
-import net.minecraft.data.client.ItemModelGenerator;
-import net.minecraft.data.client.Models;
-
-import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
-
 public class ModModelProvider extends FabricModelProvider {
-	public ModModelProvider(FabricDataOutput output) {
-		super(output);
-	}
+    public ModModelProvider(FabricDataOutput output) {
+        super(output);
+    }
 
-	/**
-	 * Generate block models and block states
-	 */
-	@Override
-	public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
-		ModModelGenerator generator = new ModModelGenerator(blockStateModelGenerator);
-		for (ArrowMarkVariant arrowMark : arrowMarks) {
-			generator.registerFlat9Direction(arrowMark.block);
-		}
-	}
+    /**
+     * Generate block models and block states
+     */
+    @Override
+    public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
+        ModModelGenerator generator = new ModModelGenerator(blockStateModelGenerator);
+        for (ArrowMarkVariant arrowMark : arrowMarks) {
+            generator.registerFlat9Direction(arrowMark.block);
+        }
+    }
 
-	/**
-	 * Generate item models
-	 */
-	@Override
-	public void generateItemModels(ItemModelGenerator itemModelGenerator) {
-		for (ArrowMarkVariant arrowMark : arrowMarks) {
-			itemModelGenerator.register(arrowMark.item, Models.GENERATED);
-		}
-	}
+    /**
+     * Generate item models
+     */
+    @Override
+    public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+        for (ArrowMarkVariant arrowMark : arrowMarks) {
+            itemModelGenerator.register(arrowMark.item, Models.GENERATED);
+        }
+    }
 }
