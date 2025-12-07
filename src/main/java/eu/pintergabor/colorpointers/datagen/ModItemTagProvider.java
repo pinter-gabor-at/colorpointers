0f6815c0050257edfadff9235e3905c3b63f2425
Pintér Gábor <pinter.gabor@gmx.at>
2025-12-07 08:53:58 +0100

    Bump versions.

--- src/main/java/eu/pintergabor/colorpointers/datagen/ModItemTagProvider.java
+++ src/main/java/eu/pintergabor/colorpointers/datagen/ModItemTagProvider.java
@@ -5,30 +5,31 @@ import java.util.concurrent.CompletableFuture;
 
 import eu.pintergabor.colorpointers.Global;
 import eu.pintergabor.colorpointers.main.Main;
-
-import net.minecraft.core.HolderLookup;
-import net.minecraft.data.PackOutput;
-import net.minecraft.data.tags.ItemTagsProvider;
-import net.minecraft.data.tags.TagsProvider;
-import net.minecraft.world.level.block.Block;
-
 import org.jetbrains.annotations.NotNull;
 
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.KeyTagProvider;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 
-public final class ModItemTagProvider extends ItemTagsProvider {
 
+public final class ModItemTagProvider extends KeyTagProvider<Item> {
+
+	@SuppressWarnings("unused")
 	public ModItemTagProvider(
 		PackOutput output,
 		CompletableFuture<HolderLookup.Provider> lookupProvider,
-		CompletableFuture<TagsProvider.TagLookup<Block>> blockTagProvider
+		CompletableFuture<TagLookup<Block>> blockTagProvider
 	) {
-		super(output, lookupProvider, blockTagProvider, Global.MODID);
+		super(output, Registries.ITEM, lookupProvider, Global.MODID);
 	}
 
 	@Override
 	protected void addTags(HolderLookup.@NotNull Provider provider) {
 		Arrays.stream(Main.arrowMarks)
 			.forEach(mark ->
-				tag(Main.ARROW_MARK_ITEM_TAG).add(mark.item.get()));
+				tag(Main.ARROW_MARK_ITEM_TAG).add(mark.item.getKey()));
 	}
 }
